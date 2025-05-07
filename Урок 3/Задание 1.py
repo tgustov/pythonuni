3d79a24a18033c3b67c27c1927d146b184510b46
ngustov <tgustova@proton.me>
2025-05-07 21:12:51 +0500

    Lesson 3 end

---
+++ Урок 3/Задание 1.py
@@ -0,0 +1,5 @@
+species = input("Вид питомца:")
+age = input("Возраст питомца:")
+name = input("Имя питомца:")
+
+print(f"Это {species} по кличке '{name}'. Возраст: {age}")
---
+++ Урок 3/Задание 2.py
@@ -0,0 +1,13 @@
+# Dryopithecus, Ramapithecus, Australopithecus,
+# Homo Habilis, Homo Erectus, Homo Sapiens Neanderthalensis, Homo Sapiens Sapiens.
+
+one = input("Первый этап развития человека ")
+two = input("Второй этап развития человека ")
+three = input("Третий этап развития человека ")
+four = input("Четвертый этап развития человека ")
+five = input("Пятый этап развития человека ")
+six = input("Шестой этап развития человека ")
+seven = input("Седьмой этап развития человека ")
+
+print(one, two, three, four, five, six, seven, sep='=>')
+print("Верный ответ: Dryopithecus=>Ramapithecus=>Australopithecus=>Homo Habilis=>Homo Erectus=>Homo Sapiens Neanderthalensis=>Homo Sapiens Sapiens.")
