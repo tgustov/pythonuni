c154fa5cf7fe5a7e42c8469957510ef446689e35
ngustov <tgustova@proton.me>
2025-06-20 16:48:20 +0500

    Добавлена генерация рек

--- HelicopterGame/map.py
+++ HelicopterGame/map.py
@@ -1,17 +1,34 @@
+from utils import randbool
+from utils import randcell
+from utils import randcell2
+
 # 0 - поле
 # 1 - дерево
 # 2 - река
 # 3 - госпиталь
 # 4 - апгрейд-шоп
 
-CELL_TYPES = "🟩🌳🌊🏥🏬"
+CELL_TYPES = "🟩🌲🌊🏥🏬"
 
 class Map:
-    def generate_rivers(self):
-        return
+    def generate_river(self, l):
+        rc = randcell(self.w, self.h)
+        rx, ry = rc[0], rc[1]
+        self.cells[rx][ry] = 2
+        while l > 0:
+            rc2 = randcell2(rx, ry)
+            rx2, ry2 = rc2[0], rc2[1]
+            if (self.check_bounds(rx2, ry2)):
+                self.cells[rx2][ry2] = 2
+                rx, ry = rx2, ry2
+                l -= 1
 
-    def generate_forest(self):
-        return
+    def generate_forest(self, r, mxr):
+        for ri in range (self.h):
+            for ci in range (self.w):
+                if randbool(r, mxr):
+                    self.cells[ri][ci] = 1
+
 
     def print_map(self):
         print(("⬛") * (self.w + 2))
@@ -33,6 +50,10 @@ class Map:
         self.h = h
         self.cells = [[0 for i in range (w)] for j in range(h)]
 
-tmp = Map(10, 10)
-tmp.cells[1][1] = 2
+tmp = Map(20, 20)
+tmp.generate_forest(3, 10)
+tmp.generate_river(10)
+tmp.generate_river(10)
+tmp.generate_river(10)
+tmp.generate_river(10)
 tmp.print_map()
