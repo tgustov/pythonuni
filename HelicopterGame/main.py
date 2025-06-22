2ed13708d491c36046385fc05839c74ed19ce2ea
ngustov <tgustova@proton.me>
2025-06-22 19:12:04 +0500

    Добавлен вертолет, механика тушения огней, механика улучшений

--- HelicopterGame/main.py
+++ HelicopterGame/main.py
@@ -3,24 +3,43 @@
 from map import Map
 import time
 import os
+from helicopter import Helicopter as Helico
+from pynput import keyboard
 
 TICK_SLEEP = 0.05
 TREE_UPDATE = 50
 FIRE_UPDATE = 100
-MAP_W, MAP_H = 5, 5
+MAP_W, MAP_H = 20, 10
 
 field = Map(MAP_W, MAP_H)
-field.generate_forest(3, 10)
-field.generate_river(10)
-field.generate_river(10)
-field.print_map()
+
+helico = Helico(MAP_W, MAP_H)
+
+
+MOVES = {'w': (-1, 0), 'd': (0, 1), 's': (1, 0), 'a': (0, -1),}
+
+def process_key(key, injected):
+    global helico
+    c = key.char.lower()
+    if c in MOVES.keys():
+        dx = MOVES[c][0]
+        dy = MOVES[c][1]
+        helico.move(dx, dy)
+
+
+listener = keyboard.Listener(
+    on_press=None,
+    on_release=process_key)
+listener.start()
 
 tick = 1
 
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     print("TICK", tick)
-    field.print_map()
+    field.process_helicopter(helico)
+    helico.print_stats()
+    field.print_map(helico)
     tick +=1
     time.sleep(TICK_SLEEP)
     if (tick % TREE_UPDATE == 0):
